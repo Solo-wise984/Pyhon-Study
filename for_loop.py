89ffd4389fd2ad65116475c651357edf025686ea
Solo-wise984 <solomonmacharia05@gmail.com>
2024-10-29 16:50:25 +0300

    range

---
+++ for_loop.py
@@ -0,0 +1,29 @@
+fruits=['apple', 'orange','mango','ovacado','bannana']
+# for fruit in fruits:
+#     print(fruit)
+for fruit in fruits:
+    if fruit=='mango':
+        print(fruit)
+
+
+#range functions
+y=list(range(1,101))
+print(y)
+
+#iterator numbers 
+x=list(range(10,101))
+for i in x:
+    print(i)
+    
+number=list(range(20,101))
+
+for num in number:
+    if num%2==0:
+        even_number.append(i)
+print(even_number)
+    
+
+
+        
+    
+    
